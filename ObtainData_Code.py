f0cf75d01ec02994661af51e6638526c63272c44
ethchr <christensen.ej@gmail.com>
2017-11-15 18:21:14 -0800

    atom-trial
    Trying from atom

--- ObtainData_Code.py
+++ ObtainData_Code.py
@@ -2,7 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-##Okay, this bit all works. url is a conference page with links, and url1 is a specific talk with text. 
+##Okay, this bit all works. url is a conference page with links, and url1 is a specific talk with text.
 #These are just me working through the process, I will need to run some sort of loop on conference page links to get each individual talk.
 url = 'https://www.lds.org/general-conference/2017/04?lang=eng'
 url1 = 'https://www.lds.org/general-conference/2017/04/gathering-the-family-of-god?lang=eng'
@@ -17,7 +17,7 @@ soup1 = BeautifulSoup(html1, "lxml")
 
 soup.title
 soup1.title
-#Okay, so we see that each one has actually got the page we wanted. 
+#Okay, so we see that each one has actually got the page we wanted.
 
 talktiles = soup.find_all("div", class_="lumen-tile lumen-tile--horizontal lumen-tile--list")
 len(talktiles)
@@ -30,7 +30,7 @@ for link in soup.find_all('a'):
 prefix = "www.lds.org"
 #All of the talk links need to have this prefix added before being used to scrape text.
 
-####The code below is based on some demos, but it doesn't work yet for me. 
+####The code below is based on some demos, but it doesn't work yet for me.
 #talksurls = {}
 #for element in talktiles:
 #    talksurls[element.a.get_text()]
@@ -38,10 +38,11 @@ prefix = "www.lds.org"
 #    talksurls[element.a.get_text()]["link"] = prefix + element.a["href"]
 #for item in talksurls.keys():
 #    print(item + ": " + "\n\t" + "link: " + talksurls[item]["link"] + "\n\n")
-#### Ideally, that would give me a dictionary with each url I need from that conference. 
+#### Ideally, that would give me a dictionary with each url I need from that conference.
 
 
 ############Just reading in the text:
 print(soup1.get_text())
 #This gets the text, and a whole lot more. Including conference (but NOT session), speaker, speaker's title, talk title, and topic tags assigned to talk on lds.org. The problem is that it includes a ton of extra stuff. So I'll need to pare away all the other stuff to get just the text, but at the same time dive into the other stuff to get those other parts. Will be a little tricky but we'll see.
 
+#####Trying to save from ATOM back up to the github
